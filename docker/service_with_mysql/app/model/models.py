a325ac2c73e138689c5df456ba38f4602cd553de
KennethanCeyer <kenneth@pigno.se>
2024-01-15 09:32:03 +0900

    feat: add service with mysql code

---
+++ docker/service_with_mysql/app/model/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String
+from pydantic import BaseModel
+
+from model.base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), index=True)
+    email = Column(String(255), unique=True, index=True)
+
+
+class UserCreate(Base.Model):
+    name: str
+    email: str
+
+
+class UserOut(BaseModel):
+    id: int
+    name: str
+    email: str
