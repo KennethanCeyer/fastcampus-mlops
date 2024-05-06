b1b69eec0431c49099e88969cc1b89298fe1b7b3
KennethanCeyer <kenneth@pigno.se>
2024-05-06 03:50:37 -0400

    feat: change to dataflow

--- dataflow/dags/dataflow.py
+++ dataflow/dags/dataflow.py
@@ -1,13 +1,16 @@
 from airflow.decorators import dag, task
 from airflow.utils.dates import days_ago
-from airflow.providers.apache.beam.operators.beam import BeamRunPythonPipelineOperator
 from airflow.providers.google.cloud.hooks.base import GoogleBaseHook
+from airflow.providers.google.cloud.operators.dataflow import (
+    DataflowCreatePythonJobOperator,
+)
+
 
 @dag(
     schedule_interval="@daily",
     start_date=days_ago(1),
     catchup=False,
-    tags=['example', 'dataflow']
+    tags=["example", "dataflow"],
 )
 def dataflow_example():
     @task
@@ -18,21 +21,25 @@ def dataflow_example():
 
     @task
     def start_dataflow_job(project_id: str):
-        start_python_job = BeamRunPythonPipelineOperator(
-            runner=BeamRunnerType.DataflowRunner,
-            task_id="start_python_job",
+        dataflow_job = DataflowCreatePythonJobOperator(
+            task_id="start_dataflow_job",
             py_file="gs://fast_campus_data_pipeline_example/scripts/dataflow_gcs_to_bigquery.py",
-            py_options=[],
-            pipeline_options={
+            options={
                 "input": "gs://fast_campus_data_pipeline_example/sample_data/language.txt",
                 "output": f"{project_id}:dataflow.languages",
+                "project": project_id,
+                "region": "us-central1",
             },
             py_requirements=["apache-beam[gcp]==2.47.0"],
             py_interpreter="python3",
             py_system_site_packages=False,
-            dataflow_config={"location": "us-central1", "job_name": "start_python_job"},
+            job_name="start_dataflow_job",
+            location="us-central1",
         )
-        return start_python_job.execute({})
+        dataflow_job.execute({})
+
+    project_id = get_gcp_project()
+    start_dataflow_job(project_id)
 
     project_id = get_gcp_project()
     start_dataflow_job(project_id)
