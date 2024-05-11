3e5acb22d12f7279a212a09a4a074d6887b958ac
KennethanCeyer <kenneth@pigno.se>
2024-05-10 20:47:30 -0700

    fix: omit tests

--- testing/introduction/tests/test_add.py
+++ testing/introduction/tests/test_add.py
@@ -1,16 +1,16 @@
 import pytest
 from add import add
 
+
 @pytest.mark.parametrize(
-   "given, expected",
-   [
-       ((1, 3), 4),
-       ((2, 7), 9),
-       ((-1, 4), 3),
-       ((0, 0), 0),
-   ]
+    "given, expected",
+    [
+        ((1, 3), 4),
+        ((2, 7), 9),
+        ((-1, 4), 3),
+        ((0, 0), 0),
+    ],
 )
 def test_add(given: tuple[int, int], expected: int):
-   result = add(*given)
-   assert result == expected
-
+    result = add(*given)
+    assert result == expected
