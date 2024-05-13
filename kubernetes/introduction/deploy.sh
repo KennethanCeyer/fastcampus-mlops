e7a0737fe369f43385ec3d968d9fa85aec373df9
KennethanCeyer <kenneth@pigno.se>
2024-05-13 06:52:26 -0700

    fix: docker issue

--- kubernetes/introduction/deploy.sh
+++ kubernetes/introduction/deploy.sh
@@ -1,3 +1,3 @@
 kubectl create namespace fast-api-example
-kubectl apply -f deployment.yml
-kubectl apply -f service.yml
+kubectl apply -f deployment/deployment.yaml
+kubectl apply -f deployment/service.yaml
